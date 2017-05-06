3940ed7040b864433fdab0897ea0408d0ab79817
niksolaz <solazzo.nicola@gmail.com>
2017-05-06 15:48:41 +0200

    added method to read only directory

---
+++ scripts/albums.js
@@ -0,0 +1,64 @@
+//require module http
+var http = require('http');
+var fs = require('fs');
+
+function load_album_list(callback){
+	fs.readdir(
+		"albums",
+		function(err,files){
+
+			if(err){
+				callback(err);
+				return;
+			}
+
+			var only_dirs = [];
+
+			(function iterator(index){
+				if (index == files.length){
+					callback(null, only_dirs);
+					return;
+				}
+
+				fs.stat(
+					"albums/" + files[index],
+					function(err,stats){
+						if(err){
+							callback(err);
+							return;
+						}
+						if(stats.isDirectory()){
+							only_dirs.push(files[index]);
+						}
+						iterator(index + 1);
+					}
+				);
+			})(0);
+			
+		}
+	);
+}
+
+
+function handle_incoming_request( req, res ){
+
+	console.log("INCOMING REQUEST: " +  req.method + " " + req.url );
+	load_album_list(function(err, albums){
+		if(err){
+			res.writeHead(503, {"Content-Type": "application/json"});
+			res.end(JSON.stringify(err) + "\n");
+			return;
+		}
+		var out = { error : null,
+					data: { albums: albums}};
+		res.writeHead(200, { "Content-Type":"application/json"});
+		res.end(JSON.stringify(out) + "\n");
+	});
+}
+
+
+var app = http.createServer( handle_incoming_request );
+var port = 8000;
+app.listen( port, function(){
+	console.log('Connected to localhost: ' + port);
+});
